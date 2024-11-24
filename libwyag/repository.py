215a62c87aa9cc0ba01a5a68e3d254f4f28a3def
zll600 <3400692417@qq.com>
2024-11-24 10:35:33 +0800

    split wyag core functionaility

--- libwyag/repository.py
+++ libwyag/repository.py
@@ -1,4 +1,3 @@
-import argparse
 import collections
 import configparser
 from datetime import datetime
@@ -15,115 +14,6 @@ import io
 import typing
 
 WYAG_DIR = '.wyag'
-
-argparser = argparse.ArgumentParser(description="The stupidest content tracker")
-argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
-argsubparsers.required = True
-
-# init
-argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
-argsp.add_argument("path",
-                   metavar="directory",
-                   nargs="?",
-                   default=".",
-                   help="Where to create the repository.")
-
-# cat-file
-argsp = argsubparsers.add_parser("cat-file",
-                                 help="Provide content of repository objects")
-argsp.add_argument("type",
-                   metavar="type",
-                   choices=["blob", "commit", "tag", "tree"],
-                   help="Specify the type")
-argsp.add_argument("object",
-                   metavar="object",
-                   help="The object to display")
-# hash-object
-argsp = argsubparsers.add_parser(
-    "hash-object",
-    help="Compute object ID and optionally creates a blob from a file")
-
-argsp.add_argument("-t",
-                   metavar="type",
-                   dest="type",
-                   choices=["blob", "commit", "tag", "tree"],
-                   default="blob",
-                   help="Specify the type")
-
-argsp.add_argument("-w",
-                   dest="write",
-                   action="store_true",
-                   help="Actually write the object into the database")
-
-argsp.add_argument("path",
-                   help="Read object from <file>")
-
-# log
-argsp = argsubparsers.add_parser("log", help="Display history of a given commit.")
-argsp.add_argument("commit",
-                   default="HEAD",
-                   nargs="?",
-                   help="Commit to start at.")
-
-# ls-tree
-argsp = argsubparsers.add_parser("ls-tree", help="Pretty-print a tree object.")
-argsp.add_argument("-r",
-                   dest="recursive",
-                   action="store_true",
-                   help="Recurse into sub-trees")
-argsp.add_argument("tree",
-                   help="A tree-ish object.")
-
-# checkout
-argsp = argsubparsers.add_parser("checkout", help="Checkout a commit inside of a directory.")
-argsp.add_argument("commit",
-                   help="The commit or tree to checkout.")
-argsp.add_argument("path",
-                   help="The EMPTY directory to checkout on.")
-# show-ref
-argsp = argsubparsers.add_parser("show-ref", help="List references.")
-
-# tag
-argsp = argsubparsers.add_parser(
-    "tag",
-    help="List and create tags")
-argsp.add_argument("-a",
-                   action="store_true",
-                   dest="create_tag_object",
-                   help="Whether to create a tag object")
-argsp.add_argument("name",
-                   nargs="?",
-                   help="The new tag's name")
-argsp.add_argument("object",
-                   default="HEAD",
-                   nargs="?",
-                   help="The object the new tag will point to")
-
-# rev-parse
-argsp = argsubparsers.add_parser(
-    "rev-parse",
-    help="Parse revision (or other objects) identifiers")
-
-argsp.add_argument("--wyag-type",
-                   metavar="type",
-                   dest="type",
-                   choices=["blob", "commit", "tag", "tree"],
-                   default=None,
-                   help="Specify the expected type")
-
-argsp.add_argument("name",
-                   help="The name to parse")
-
-# ls-files
-argsp = argsubparsers.add_parser("ls-files", help = "List all the stage files")
-argsp.add_argument("--verbose", action="store_true", help="Show everything.")
-
-# check-ignore
-argsp = argsubparsers.add_parser("check-ignore", help = "Check path(s) against ignore rules.")
-argsp.add_argument("path", nargs="+", help="Paths to check")
-
-# status
-argsp = argsubparsers.add_parser("status", help = "Show the working tree status.")
 
 def repo_path(repo: typing.ForwardRef('GitRepository'), *path) -> str:
     """Compute path under repo's gitdir."""
@@ -983,7 +873,7 @@ def cmd_show_ref(args):
     show_ref(repo, refs, prefix="refs")
 
 def ref_create(repo: GitRepository, ref_name: str, sha: str):
-    with open(repo_file(repo, "refs/" + ref_name), 'w') as fp:
+    with open(repo_file(repo, "refs/" + ref_name), 'w', encoding='utf-8') as fp:
         fp.write(sha + "\n")
 
 def tag_create(repo: GitRepository, name: str, ref: str, create_tag_object=False, type=None):
@@ -1167,7 +1057,7 @@ def cmd_check_ignore(args):
         if check_ignore(rules, path):
             print(path)
 
-def branch_get_active(repo: GitRepository) -> str|False:
+def branch_get_active(repo: GitRepository) -> str|bool:
     with open(repo_file(repo, "HEAD"), "r", encoding='utf-8') as f:
         head = f.read()
 
@@ -1291,9 +1181,6 @@ def cmd_status(_):
     print()
     cmd_status_index_worktree(repo, index)
 
-argsp = argsubparsers.add_parser("rm", help="Remove files from the working tree and the index.")
-argsp.add_argument("path", nargs="+", help="Files to remove")
-
 def rm(repo: GitRepository, paths: list[str], delete: bool=True, skip_missing: bool=False):
     # Find and read the index
     index = index_read(repo)
@@ -1334,9 +1221,6 @@ def rm(repo: GitRepository, paths: list[str], delete: bool=True, skip_missing: b
 def cmd_rm(args):
     repo = repo_find()
     rm(repo, args.path)
-
-argsp = argsubparsers.add_parser("add", help = "Add files contents to the index.")
-argsp.add_argument("path", nargs="+", help="Files to add")
 
 def add(repo: GitRepository, paths: list[str], delete: bool=True, skip_missing: bool=False) -> None:
     # First remove all paths from the index, if they exist.
@@ -1385,12 +1269,6 @@ def add(repo: GitRepository, paths: list[str], delete: bool=True, skip_missing: 
 def cmd_add(args):
     repo = repo_find()
     add(repo, args.path)
-
-argsp = argsubparsers.add_parser("commit", help="Record changes to the repository.")
-argsp.add_argument("-m",
-                   metavar="message",
-                   dest="message",
-                   help="Message to associate with this commit.")
 
 def gitconfig_read():
     xdg_config_home = os.environ["XDG_CONFIG_HOME"] if "XDG_CONFIG_HOME" in os.environ else "~/.config"
@@ -1516,23 +1394,3 @@ def cmd_commit(args):
     else: # Otherwise, we update HEAD itself.
         with open(repo_file(repo, "HEAD"), "w", encoding='utf-8') as fd:
             fd.write("\n")
-
-def main(argv=sys.argv[1:]):
-    args = argparser.parse_args(argv)
-    match args.command:
-        case "add"          : cmd_add(args)
-        case "cat-file"     : cmd_cat_file(args)
-        case "check-ignore" : cmd_check_ignore(args)
-        case "checkout"     : cmd_checkout(args)
-        case "commit"       : cmd_commit(args)
-        case "hash-object"  : cmd_hash_object(args)
-        case "init"         : cmd_init(args)
-        case "log"          : cmd_log(args)
-        case "ls-files"     : cmd_ls_files(args)
-        case "ls-tree"      : cmd_ls_tree(args)
-        case "rev-parse"    : cmd_rev_parse(args)
-        case "rm"           : cmd_rm(args)
-        case "show-ref"     : cmd_show_ref(args)
-        case "status"       : cmd_status(args)
-        case "tag"          : cmd_tag(args)
-        case _              : print("Bad command.")
---
+++ libwyag/wyag.py
@@ -0,0 +1,149 @@
+import argparse
+import sys
+from libwyag.repository import cmd_add, cmd_commit, cmd_cat_file, cmd_check_ignore, cmd_rm, cmd_checkout, cmd_hash_object, cmd_init, cmd_log, cmd_ls_files, cmd_ls_tree, cmd_rev_parse, cmd_show_ref, cmd_status, cmd_tag
+
+WYAG_DIR = '.wyag'
+
+argparser = argparse.ArgumentParser(description="The stupidest content tracker")
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
+argsubparsers.required = True
+
+# init
+argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
+argsp.add_argument("path",
+                   metavar="directory",
+                   nargs="?",
+                   default=".",
+                   help="Where to create the repository.")
+
+# cat-file
+argsp = argsubparsers.add_parser("cat-file",
+                                 help="Provide content of repository objects")
+argsp.add_argument("type",
+                   metavar="type",
+                   choices=["blob", "commit", "tag", "tree"],
+                   help="Specify the type")
+argsp.add_argument("object",
+                   metavar="object",
+                   help="The object to display")
+# hash-object
+argsp = argsubparsers.add_parser(
+    "hash-object",
+    help="Compute object ID and optionally creates a blob from a file")
+
+argsp.add_argument("-t",
+                   metavar="type",
+                   dest="type",
+                   choices=["blob", "commit", "tag", "tree"],
+                   default="blob",
+                   help="Specify the type")
+
+argsp.add_argument("-w",
+                   dest="write",
+                   action="store_true",
+                   help="Actually write the object into the database")
+
+argsp.add_argument("path",
+                   help="Read object from <file>")
+
+# log
+argsp = argsubparsers.add_parser("log", help="Display history of a given commit.")
+argsp.add_argument("commit",
+                   default="HEAD",
+                   nargs="?",
+                   help="Commit to start at.")
+
+# ls-tree
+argsp = argsubparsers.add_parser("ls-tree", help="Pretty-print a tree object.")
+argsp.add_argument("-r",
+                   dest="recursive",
+                   action="store_true",
+                   help="Recurse into sub-trees")
+argsp.add_argument("tree",
+                   help="A tree-ish object.")
+
+# checkout
+argsp = argsubparsers.add_parser("checkout", help="Checkout a commit inside of a directory.")
+argsp.add_argument("commit",
+                   help="The commit or tree to checkout.")
+argsp.add_argument("path",
+                   help="The EMPTY directory to checkout on.")
+# show-ref
+argsp = argsubparsers.add_parser("show-ref", help="List references.")
+
+# tag
+argsp = argsubparsers.add_parser(
+    "tag",
+    help="List and create tags")
+argsp.add_argument("-a",
+                   action="store_true",
+                   dest="create_tag_object",
+                   help="Whether to create a tag object")
+argsp.add_argument("name",
+                   nargs="?",
+                   help="The new tag's name")
+argsp.add_argument("object",
+                   default="HEAD",
+                   nargs="?",
+                   help="The object the new tag will point to")
+
+# rev-parse
+argsp = argsubparsers.add_parser(
+    "rev-parse",
+    help="Parse revision (or other objects) identifiers")
+
+argsp.add_argument("--wyag-type",
+                   metavar="type",
+                   dest="type",
+                   choices=["blob", "commit", "tag", "tree"],
+                   default=None,
+                   help="Specify the expected type")
+
+argsp.add_argument("name",
+                   help="The name to parse")
+
+# ls-files
+argsp = argsubparsers.add_parser("ls-files", help = "List all the stage files")
+argsp.add_argument("--verbose", action="store_true", help="Show everything.")
+
+# check-ignore
+argsp = argsubparsers.add_parser("check-ignore", help = "Check path(s) against ignore rules.")
+argsp.add_argument("path", nargs="+", help="Paths to check")
+
+# status
+argsp = argsubparsers.add_parser("status", help = "Show the working tree status.")
+
+# rm
+argsp = argsubparsers.add_parser("rm", help="Remove files from the working tree and the index.")
+argsp.add_argument("path", nargs="+", help="Files to remove")
+
+# add
+argsp = argsubparsers.add_parser("add", help = "Add files contents to the index.")
+argsp.add_argument("path", nargs="+", help="Files to add")
+
+# commit
+argsp = argsubparsers.add_parser("commit", help="Record changes to the repository.")
+argsp.add_argument("-m",
+                   metavar="message",
+                   dest="message",
+                   help="Message to associate with this commit.")
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+    match args.command:
+        case "add"          : cmd_add(args)
+        case "cat-file"     : cmd_cat_file(args)
+        case "check-ignore" : cmd_check_ignore(args)
+        case "checkout"     : cmd_checkout(args)
+        case "commit"       : cmd_commit(args)
+        case "hash-object"  : cmd_hash_object(args)
+        case "init"         : cmd_init(args)
+        case "log"          : cmd_log(args)
+        case "ls-files"     : cmd_ls_files(args)
+        case "ls-tree"      : cmd_ls_tree(args)
+        case "rev-parse"    : cmd_rev_parse(args)
+        case "rm"           : cmd_rm(args)
+        case "show-ref"     : cmd_show_ref(args)
+        case "status"       : cmd_status(args)
+        case "tag"          : cmd_tag(args)
+        case _              : print("Bad command.")
