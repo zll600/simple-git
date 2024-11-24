89dfcbe4392e525a92cd111250fabdeaa94e7def
zll600 <3400692417@qq.com>
2024-11-24 16:11:22 +0800

    complete

--- libwyag/repository.py
+++ libwyag/repository.py
@@ -154,25 +154,28 @@ class GitObject(object):
         else:
             self.init()
 
-    def serialize(self, repo) -> str:
+    def serialize(self, repo: GitRepository) -> str:
         """This function MUST be implemented by subclasses.
 
-        It must read the object's contents from self.data, a byte string, and do
-        whatever it takes to convert it into a meaningful representation.  What exactly that means depend on each subclass.
+        It must read the object's contents from self.data, a byte string,
+        and do whatever it takes to convert it into a meaningful representation.
+        What exactly that means depend on each subclass.
         """
-        raise Exception("Unimplemented!")
+        raise NotImplementedError("Unimplemented!")
 
     def deserialize(self, data):
-        raise Exception("Unimplemented!")
+        raise NotImplementedError("Unimplemented!")
 
     def init(self):
         pass  # Just do nothing. This is a reasonable default!
 
 
 class GitBlob(GitObject):
-    fmt = b"blob"
+    def __init__(self, data: str) -> typing.Self:
+        self.format_str = b"blob"
+        super().__init__(data)
 
-    def serialize(self, repo: GitRepository):
+    def serialize(self, repo: GitRepository) -> str:
         return self.blobdata
 
     def deserialize(self, data):
@@ -260,7 +263,10 @@ def kvlm_serialize(kvlm: collections.OrderedDict):
 
 
 class GitCommit(GitObject):
-    fmt = b"commit"
+    def __init__(self, data: str) -> typing.Self:
+        self.format_str = b"commit"
+        self.kvlm = None
+        super().__init__(data)
 
     def deserialize(self, data):
         self.kvlm = kvlm_parse(data)
@@ -273,7 +279,7 @@ class GitCommit(GitObject):
 
 
 class GitTreeLeaf(object):
-    def __init__(self, mode: str, path: str, sha: str):
+    def __init__(self, mode: str, path: str, sha: str) -> typing.Self:
         self.mode = mode
         self.path = path
         self.sha = sha
@@ -325,22 +331,10 @@ def tree_leaf_sort_key(leaf: GitTreeLeaf) -> str:
     else:
         return leaf.path + "/"
 
-
-def tree_serialize(obj):
-    obj.items.sort(key=tree_leaf_sort_key)
-    ret = b""
-    for i in obj.items:
-        ret += i.mode
-        ret += b" "
-        ret += i.path.encode("utf8")
-        ret += b"\x00"
-        sha = int(i.sha, 16)
-        ret += sha.to_bytes(20, byteorder="big")
-    return ret
-
-
 class GitTree(GitObject):
-    fmt = b"tree"
+    def __init__(self, data: str) -> typing.Self:
+        self.format_str = b"tree"
+        super().__init__(data)
 
     def deserialize(self, data):
         self.items = tree_parse(data)
@@ -350,6 +344,18 @@ class GitTree(GitObject):
 
     def init(self):
         self.items = list()
+
+def tree_serialize(obj: GitTree) -> str:
+    obj.items.sort(key=tree_leaf_sort_key)
+    ret = b""
+    for i in obj.items:
+        ret += i.mode
+        ret += b" "
+        ret += i.path.encode("utf-8")
+        ret += b"\x00"
+        sha = int(i.sha, 16)
+        ret += sha.to_bytes(20, byteorder="big")
+    return ret
 
 
 def ref_resolve(repo: GitRepository, ref: str) -> str:
@@ -389,10 +395,10 @@ def ref_list(repo: GitRepository, path: str = None) -> collections.OrderedDict:
 
 
 class GitTag(GitCommit):
-    fmt = b"tag"
+    format_str = b"tag"
 
 
-class GitIndexEntry(object):
+class GitIndexEntry:
     def __init__(
         self,
         ctime=None,
@@ -438,11 +444,7 @@ class GitIndexEntry(object):
         self.name = name
 
 
-class GitIndex(object):
-    version = None
-    entries = []
-    # ext = None
-    # sha = None
+class GitIndex:
 
     def __init__(self, version=2, entries=None):
         if not entries:
@@ -450,6 +452,8 @@ class GitIndex(object):
 
         self.version = version
         self.entries: list[GitIndexEntry] = entries
+        # self.ext = None
+        # self.sha = None
 
 
 def index_read(repo: GitRepository) -> GitIndex:
@@ -646,32 +650,32 @@ def object_read(repo: GitRepository, sha: str) -> GitObject:
         y = raw.find(b"\x00", x)
         size = int(raw[x:y].decode("ascii"))
         if size != len(raw) - y - 1:
-            raise Exception("Malformed object {0}: bad length".format(sha))
+            raise RuntimeError(f"Malformed object {sha}: bad length")
 
         # Pick constructor
         match fmt:
             case b"commit":
-                c = GitCommit
+                git_object_type = GitCommit
             case b"tree":
-                c = GitTree
+                git_object_type = GitTree
             case b"tag":
-                c = GitTag
+                git_object_type = GitTag
             case b"blob":
-                c = GitBlob
+                git_object_type = GitBlob
             case _:
-                raise Exception(
-                    "Unknown type {0} for object {1}".format(fmt.decode("ascii"), sha)
-                )
+                raise RuntimeError(f"Unknown type {fmt.decode("ascii")} for object {sha}")
 
         # Call constructor and return object
-        return c(raw[y + 1 :])
+        return git_object_type(raw[y + 1 :])
 
 
-def object_write(obj, repo: GitRepository = None) -> str:
+def object_write(obj: GitObject, repo: GitRepository = None) -> str:
     # Serialize object data
-    data = obj.serialize()
+    data = obj.serialize(repo)
+
     # Add header
-    result = obj.fmt + b" " + str(len(data)).encode() + b"\x00" + data
+    result = obj.format_str + b" " + str(len(data)).encode() + b"\x00" + data
+
     # Compute hash
     sha = hashlib.sha1(result).hexdigest()
 
@@ -743,10 +747,10 @@ def object_find(repo: GitRepository, name: str, fmt=None, follow=True) -> str:
     sha: list[str] = object_resolve(repo, name)
 
     if not sha:
-        raise Exception("No such reference {0}.".format(name))
+        raise RuntimeError(f"No such reference {name}.")
 
     if len(sha) > 1:
-        raise Exception(
+        raise RuntimeError(
             "Ambiguous reference {0}: Candidates are:\n - {1}.".format(
                 name, "\n - ".join(sha)
             )
@@ -764,16 +768,16 @@ def object_find(repo: GitRepository, name: str, fmt=None, follow=True) -> str:
         # that in a loop, albeit normally short.  Don't expect
         # high performance here.
 
-        if obj.fmt == fmt:
+        if obj.format_str == fmt:
             return sha
 
         if not follow:
             return None
 
         # Follow tags
-        if obj.fmt == b"tag":
+        if obj.format_str == b"tag":
             sha = obj.kvlm[b"object"].decode("ascii")
-        elif obj.fmt == b"commit" and fmt == b"tree":
+        elif obj.format_str == b"commit" and fmt == b"tree":
             sha = obj.kvlm[b"tree"].decode("ascii")
         else:
             return None
@@ -784,17 +788,17 @@ def cat_file(repo: GitRepository, obj: str, fmt=None):
     sys.stdout.buffer.write(git_obj.serialize(repo))
 
 
-def cmd_cat_file(args):
+def cmd_cat_file(args: argparse.Namespace):
     repo = repo_find()
     cat_file(repo, args.object, fmt=args.type.encode())
 
 
-def object_hash(fd: io.BufferedReader, fmt: str, repo=None) -> str:
+def object_hash(fd: io.BufferedReader, format_str: str, repo=None) -> str:
     """Hash object, writing it to repo if provided."""
     data = fd.read()
 
     # Choose constructor according to fmt argument
-    match fmt:
+    match format_str:
         case b"commit":
             obj = GitCommit(data)
         case b"tree":
@@ -804,12 +808,12 @@ def object_hash(fd: io.BufferedReader, fmt: str, repo=None) -> str:
         case b"blob":
             obj = GitBlob(data)
         case _:
-            raise Exception("Unknown type %s!" % fmt)
+            raise RuntimeError(f"Unknown type {format_str}!")
 
     return object_write(obj, repo)
 
 
-def cmd_hash_object(args):
+def cmd_hash_object(args: argparse.Namespace):
     if args.write:
         repo = repo_find()
     else:
@@ -836,7 +840,7 @@ def log_graphviz(repo: str, sha: str, seen: set) -> None:
         message = message[: message.index("\n")]
 
     print('  c_{0} [label="{1}: {2}"]'.format(sha, sha[0:7], message))
-    assert commit.fmt == b"commit"
+    assert commit.format_str == b"commit"
 
     if not b"parent" in commit.kvlm.keys():
         # Base case: the initial commit.
@@ -864,32 +868,32 @@ def cmd_log(args):
 
 def ls_tree(repo: GitRepository, ref: str, recursive=None, prefix=""):
     sha = object_find(repo, ref, fmt=b"tree")
-    obj = object_read(repo, sha)
+    obj: GitTree = object_read(repo, sha)
     for item in obj.items:
         if len(item.mode) == 5:
-            type = item.mode[0:1]
+            item_type = item.mode[0:1]
         else:
-            type = item.mode[0:2]
+            item_type = item.mode[0:2]
 
-        match type:  # Determine the type.
+        match item_type:  # Determine the type.
             case b"04":
-                type = "tree"
+                item_type = "tree"
             case b"10":
-                type = "blob"  # A regular file.
+                item_type = "blob"  # A regular file.
             case b"12":
-                type = "blob"  # A symlink. Blob contents is link target.
+                item_type = "blob"  # A symlink. Blob contents is link target.
             case b"16":
-                type = "commit"  # A submodule
+                item_type = "commit"  # A submodule
             case _:
-                raise Exception("Weird tree leaf mode {}".format(item.mode))
+                raise RuntimeError(f"Weird tree leaf mode {item.node}")
 
-        if not (recursive and type == "tree"):  # This is a leaf
+        if not (recursive and item_type == "tree"):  # This is a leaf
             print(
                 "{0} {1} {2}\t{3}".format(
                     "0" * (6 - len(item.mode)) + item.mode.decode("ascii"),
                     # Git's ls-tree displays the type
                     # of the object pointed to.  We can do that too :)
-                    type,
+                    item_type,
                     item.sha,
                     os.path.join(prefix, item.path),
                 )
@@ -898,7 +902,7 @@ def ls_tree(repo: GitRepository, ref: str, recursive=None, prefix=""):
             ls_tree(repo, item.sha, recursive, os.path.join(prefix, item.path))
 
 
-def cmd_ls_tree(args):
+def cmd_ls_tree(args: argparse.Namespace):
     repo = repo_find()
     ls_tree(repo, args.tree, args.recursive)
 
@@ -908,30 +912,30 @@ def tree_checkout(repo: GitRepository, tree: GitObject, path: str) -> None:
         obj = object_read(repo, item.sha)
         dest = os.path.join(path, item.path)
 
-        if obj.fmt == b"tree":
+        if obj.format_str == b"tree":
             os.mkdir(dest)
             tree_checkout(repo, obj, dest)
-        elif obj.fmt == b"blob":
+        elif obj.format_str == b"blob":
             # @TODO Support symlinks (identified by mode 12****)
             with open(dest, "wb") as f:
                 f.write(obj.blobdata)
 
 
-def cmd_checkout(args):
+def cmd_checkout(args: argparse.Namespace):
     repo = repo_find()
 
-    obj = object_read(repo, object_find(repo, args.commit))
+    obj: GitObject = object_read(repo, object_find(repo, args.commit))
 
     # If the object is a commit, we grab its tree
-    if obj.fmt == b"commit":
+    if obj.format_str == b"commit":
         obj = object_read(repo, obj.kvlm[b"tree"].decode("ascii"))
 
     # Verify that path is an empty directory
     if os.path.exists(args.path):
         if not os.path.isdir(args.path):
-            raise Exception("Not a directory {0}!".format(args.path))
+            raise NotADirectoryError(f"Not a directory {args.path}!")
         if os.listdir(args.path):
-            raise Exception("Not empty {0}!".format(args.path))
+            raise RuntimeError(f"Not empty {args.path}!")
     else:
         os.makedirs(args.path)
 
@@ -945,7 +949,7 @@ def show_ref(
     prefix: str = "",
 ):
     for k, v in refs.items():
-        if type(v) == str:
+        if isinstance(v, str):
             print(
                 "{0}{1}{2}".format(
                     v + " " if with_hash else "", prefix + "/" if prefix else "", k
@@ -960,7 +964,7 @@ def show_ref(
             )
 
 
-def cmd_show_ref(args):
+def cmd_show_ref(args: argparse.Namespace):
     repo = repo_find()
     refs = ref_list(repo)
     show_ref(repo, refs, prefix="refs")
@@ -999,7 +1003,7 @@ def tag_create(
         ref_create(repo, "tags/" + name, sha)
 
 
-def cmd_tag(args):
+def cmd_tag(args: argparse.Namespace):
     repo = repo_find()
 
     if args.name:
@@ -1522,8 +1526,8 @@ def tree_from_index(repo: GitRepository, index: GitIndex):
     return sha
 
 
-def commit_create(repo: GitRepository, tree, parent, author, timestamp, message):
-    commit = GitCommit()  # Create the new commit object.
+def commit_create(repo: GitRepository, tree: str, parent, author, timestamp, message):
+    commit = GitCommit(tree)  # Create the new commit object.
     commit.kvlm[b"tree"] = tree.encode("ascii")
     if parent:
         commit.kvlm[b"parent"] = parent.encode("ascii")
